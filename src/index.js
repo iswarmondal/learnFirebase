2a97aa2a8a80b36ce6eb9f7e21fedc98986c57e4
Iswar <rick731202@gmail.com>
2022-05-13 09:40:32 +0530

    lession 13 done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import {initializeApp} from 'firebase/app';
 import {
-  getFirestore, collection, getDocs,
-  addDoc, doc, deleteDoc
+  getFirestore, collection, onSnapshot,
+  addDoc, doc, deleteDoc,
+  query, where, serverTimestamp, orderBy, updateDoc
 } from 'firebase/firestore'
+
+import {
+  getAuth, createUserWithEmailAndPassword,
+  signOut, signInWithEmailAndPassword
+} from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBliJvMoxcBdXHseEqlNlnXnXhmI-pOhxI",
@@ -22,16 +28,16 @@ const firebaseConfig = {
   // Get the collection referance
   const colRef = collection(db, 'books')
 
+  // FireStore query
+  const q = query(colRef, orderBy("createdAt"));
+
   // Get all the documents in that collection
-  getDocs(colRef)
-    .then((snapshot)=>{
+    onSnapshot(q, (snapshot)=>{
       let books = [];
       snapshot.docs.forEach((doc)=>{
         books.push({ id: doc.id, ...doc.data() })
       })
       console.log(books);
-    }).catch(err => {
-      throw err;
     })
 
   // Add new entry
@@ -42,7 +48,8 @@ const firebaseConfig = {
 
     addDoc(colRef, {
       title: addForm.title.value,
-      author: addForm.author.value
+      author: addForm.author.value,
+      createdAt: serverTimestamp(),
     })
       .then(() => addForm.reset())
   })
@@ -59,3 +66,70 @@ const firebaseConfig = {
         deleteFrom.reset();
       })
   })
+
+  // Update documents
+  const updateFrom = document.querySelector(".update");
+  updateFrom.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const docRef = doc(db, "books", updateFrom.id.value);
+
+    updateDoc(docRef, {
+      author: updateFrom.author.value
+    })
+      .then(()=> updateFrom.reset())
+  })
+
+// Setup firebase auth
+const auth = getAuth();
+
+// Signup form
+const signup = document.querySelector("#signup");
+signup.addEventListener('submit', (e)=>{
+  e.preventDefault();
+
+  const email = signup.email.value;
+  const pass = signup.pass.value;
+
+  createUserWithEmailAndPassword(auth, email, pass)
+    .then((credential)=>{
+      console.log(`New user credentials are ${credential}`);
+      signup.reset();
+    })
+    .catch((err)=>{
+      console.log(err.message);
+    })
+})
+
+// logging out
+const logout = document.querySelector('#logout');
+
+logout.addEventListener('click', (e)=>{
+  e.preventDefault();
+
+  signOut(auth)
+    .then(()=>{
+      console.log(`User logged out`);
+    })
+    .catch((err)=>{
+      console.log(err.message);
+    })
+})
+
+// Login user
+const login = document.querySelector("#login");
+login.addEventListener('submit', (e)=>{
+  e.preventDefault();
+
+  const email = login.email.value;
+  const pass = login.pass.value;
+
+  signInWithEmailAndPassword(auth, email, pass)
+    .then(cred=>{
+      console.log(cred.user);
+      login.reset();
+    })
+    .catch((err)=>{
+      console.log(err.message);
+    })
+})
